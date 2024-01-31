cbe9140e1d9ab15a9589baab4e1138bfa5db9da4
SairaBeg <sairabeg.dev@gmail.com>
2024-01-30 22:50:44 -0500

    Redux toolkit added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
+import { actions } from "./store/index";
 
 function App() {
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch(actions.increment());
+  };
+  const decrement = () => {
+    dispatch(actions.decrement());
+  };
+  const addBy = () => {
+    dispatch(actions.addBy());
+  };
   return (
     <div>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={addBy}>Add 10</button>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,40 @@
-import { legacy_createStore as createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const reducerFn = (state = { counter: 10 }, action) => {
-  return state;
-};
-
-const store = createStore(reducerFn);
-
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { counter: 0 },
+  reducers: {
+    increment(state, action) {
+      state.counter += 1;
+    },
+    decrement(state, action) {
+      state.counter -= 1;
+    },
+    addBy(state, action) {
+      state.counter += 10;
+    },
+  },
+});
+export const actions = counterSlice.actions;
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 export default store;
+// import { legacy_createStore as createStore } from "redux";
+
+// const reducerFn = (state = { counter: 0 }, action) => {
+//   if (action.type === "INC") {
+//     return { counter: state.counter + 1 };
+//   }
+//   if (action.type === "DEC") {
+//     return { counter: state.counter - 1 };
+//   }
+//   if (action.type === "ADD") {
+//     return { counter: state.counter + 10 };
+//   }
+//   return state;
+// };
+
+// const store = createStore(reducerFn);
+
+// export default store;
